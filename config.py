0f673ef7f615d65c008ce65868e3e6a064d00e37
RalIlieva <rilieva8820@gmail.com>
2024-03-10 00:56:41 +0200

    Fix in config file MAIL_USE_TLS = 1. Now reset password request is sending emails

--- config.py
+++ config.py
@@ -8,9 +8,10 @@ class Config:
                               'sqlite:///' + os.path.join(basedir, 'instance', 'website.db')
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is True
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or 1
     MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') is False
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'pthntstngml@gmail.com'
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'broh fsjg mvzv enje'
+    MAIL_AUTH_METHOD = 'PLAIN'  # Use PLAIN authentication
     ADMINS = ['pthntstngml@gmail.com']
     # POSTS_PER_PAGE = 5
